698a200f100ae94c0a92f7aed61251db9eaa9a21
Andrew Watts <ahwatts@gmail.com>
2021-02-13 18:51:24 -0500

    Lint fixes to .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,32 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "import",
-    "jsx-a11y",
-    "react",
-  ],
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "node": true,
+  extends: "airbnb",
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
   },
-  "rules": {
+  rules: {
     "linebreak-style": "off",
-    "quotes": [2, "double"],
-    "indent": [
+    quotes: [2, "double"],
+    indent: [
       "error",
       2,
       {
-        "SwitchCase": 0,
-        "VariableDeclarator": 1,
-        "outerIIFEBody": 1,
-        "FunctionDeclaration": {
-          "parameters": 1,
-          "body": 1
+        SwitchCase: 0,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+        FunctionDeclaration: {
+          parameters: 1,
+          body: 1,
         },
-        "FunctionExpression": {
-          "parameters": 1,
-          "body": 1
-        }
-      }
+        FunctionExpression: {
+          parameters: 1,
+          body: 1,
+        },
+      },
     ],
     "react/jsx-first-prop-new-line": "off",
     "react/jsx-closing-bracket-location": "off",
     "react/jsx-indent-props": "off",
-  }
+  },
 };
